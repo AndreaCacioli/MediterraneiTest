ed3e1df12a480042ad7577d9e3a4c1f26c41bf83
Andrea Cacioli <andrea.cacioli.1c@gmail.com>
2025-04-15 23:27:50 +0200

    Opted for 2 models fine tuned on specific tasks instead of a fundational model
    Now we use facebook/bart-large-mnli for zero-shot-classification on 2
    classes [acceptable] and [needs moderation].
    To detect spam we use
    mariagrandury/roberta-base-finetuned-sms-spam-detection for spam/ham
    recognition.

--- mediterranei_backend/api/services/llm.py
+++ mediterranei_backend/api/services/llm.py
@@ -1,27 +1,43 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, AutoModelForSequenceClassification
 import torch
 
-class LLM():
-    def __init__(self, model_name = "microsoft/phi-2"): # Do not change the model unless you are very sure it works with the same prompt structure!!!
+class LLMClassifier():
+    def __init__(self): # Do not change the model unless you are very sure it works with the same prompt structure!!!
         torch.set_default_device("cuda")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print(self.device)
+        self.moderation_classifier = pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
+        self.moderation_labels = ['needs moderation', 'acceptable']
+        
+        model_name = "mariagrandury/roberta-base-finetuned-sms-spam-detection"
+        self.spam_tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.spam_model = AutoModelForSequenceClassification.from_pretrained(model_name).to(self.device)
 
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", trust_remote_code=True).to(self.device)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    #TODO add support for batched reviews for efficiency
+    def classify_moderation(self, review):
+        result = self.moderation_classifier(review, candidate_labels = self.moderation_labels)
+        top_label = result['labels'][0]
+        confidence = result['scores'][0]
+        return top_label, confidence
 
+    def isSpam(self,text):
+        inputs = self.spam_tokenizer(text, return_tensors="pt", truncation=True)
+        with torch.no_grad():
+            outputs = self.spam_model(**inputs)
+        probabilities = torch.nn.functional.softmax(outputs.logits, dim=1)
+        predicted_label = torch.argmax(probabilities, dim=1).item()
 
+        # Map model output to custom categories
+        return predicted_label == 1
+    
 
-    def ask(self, prompt) -> str:
-        inputs = self.tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
-        outputs = self.model.generate(**inputs, max_length=200)
-        text = self.tokenizer.batch_decode(outputs)[0]
-        return text
 
 if __name__ == '__main__':
-    llm = LLM()
+    llm = LLMClassifier()
     pos_rev = "I visited this amusement park and it was a blast – the rides were thrilling and the atmosphere was festive. It was a great day out."
     neg_rev = "I visited this shitty amusement park and it was fucking awful – the rides were thrilling if you are a five year old. I want a refund... NOW!"
-    spam_rev = "CONGRATUKATIONS! You won a new IPhone! Visit my website for more information about my prices, special discounts are available here: https://andreacacioli.netlify.app/"
-
-    print(llm.ask(f"Instruct: Out of these 3 classes: (Appropriate, Inappropriate, SPAM) [ONLY PICK ONE CLASS AND ONLY WRITE A SINGLE WORD, DO NOT TELL ANYTHING ELSE], considering the following review: \n {pos_rev} \n how would a human moderator consider this? \n Output: "))
+    spam_rev = "CONGRATULATIONS! Wanna get rich quick? You won a new IPhone! Visit my website for more information about my prices, special discounts are available here: https://andreacacioli.netlify.app/"
+    for r in [pos_rev, neg_rev, spam_rev]:
+        print(r)
+        print(llm.classify_moderation(r))
+        print(llm.isSpam(r))
+        print()
